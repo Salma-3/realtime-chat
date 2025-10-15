b77de2e77f4a00a4a9f7d0c6b29591030b40ee80
Salma-3 <salmehdhar@icloud.com>
2025-10-15 18:37:52 +0400

    code refactoring

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,11 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const mongoUri = process.env.MONGO_URI;
+        if(!mongoUri) {
+            throw new Error('MONGO_URI is not set');
+        }
+        const conn = await mongoose.connect(mongoUri);
         console.log('Connected to MongoDB Server', conn.connection.host)
     } catch (error) {
         console.error('Error connecting to MongoDB:', error)
